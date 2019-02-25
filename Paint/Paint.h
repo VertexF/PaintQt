810bf7ca14378dbd32d0cb6514ef95e49134e292
VertexF <danmanley93@gmail.com>
2019-02-25 10:42:16 +0000

    Skeleton Qt Project set
    Here I just set up the skeleton of this project and linked everything up.

--- Paint/Paint.h
+++ Paint/Paint.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <QList>
 #include <QtWidgets/QMainWindow>
 #include "ui_Paint.h"
 
@@ -7,12 +8,17 @@ namespace Ui {
 	class Paint;
 }
 
+class ScribbleArea;
+
 class Paint : public QMainWindow
 {
 	Q_OBJECT
 
 public:
 	Paint(QWidget *parent = Q_NULLPTR);
+protected:
+
+protected slots:
 
 private:
 	Ui::Paint* ui;
---
+++ Paint/ScribbleArea.cpp
@@ -0,0 +1,13 @@
+#include "ScribbleArea.h"
+
+
+
+ScribbleArea::ScribbleArea(QWidget *parent)
+	: QWidget(parent)
+{
+}
+
+
+ScribbleArea::~ScribbleArea()
+{
+}
